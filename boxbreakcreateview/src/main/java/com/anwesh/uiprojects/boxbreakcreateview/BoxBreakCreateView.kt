31034210e17b62ee1767d6c395a851b2d9d4167d
Anwesh43 <anweshthecool0@gmail.com>
2019-08-16 11:25:33 +0530

    adding constants and libraries

--- boxbreakcreateview/src/main/java/com/anwesh/uiprojects/boxbreakcreateview/BoxBreakCreateView.kt
+++ boxbreakcreateview/src/main/java/com/anwesh/uiprojects/boxbreakcreateview/BoxBreakCreateView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.boxbreakcreateview
  * Created by anweshmishra on 16/08/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#E65100")
+val backColor : Int = Color.parseColor("#BDBDBD")
